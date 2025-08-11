4d68512215c324bfe34708fb5aa9a0c955dfb93f
Egide-2 <EgideBilly69@gmail.com>
2025-08-11 08:35:19 +0200

    check the  character code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 
-
-
-
-
-
-
-const words = "hello"
-const hello = words.map(wordsew[1])
-
-console.log(hello.toUppercase)
+const isVow = (arr) => {
+  return arr.map(num => {
+    if (num === 97) return "a";
+    else if (num === 101) return "e";
+    else if (num === 105) return "i";
+    else if (num === 111) return "o"; 
+    else if (num === 117) return "u";
+    else return num;
+  });
+};
