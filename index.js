2812e2c000d56631dd58352537f8fb6d22f780d0
Egide-2 <EgideBilly69@gmail.com>
2025-08-12 08:15:22 +0200

    how to remove the space between the letter of string

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 
 
 
-const countBy = (x, n) => {
-  const result = [];
-  for (let i = 1; i <= n; i++) {
-    result.push(x * i);
-  }
-  return result;
-};
+// const campany = {
+//     name:"PAGE",
+//     address: {
+//         kigale:"gikodo",
+//         East:"kirehe"},
+//     service:["health service","feed service"]
+
+// }
+    
+// for (let key in campany){
+//     console.log(key +":" + campany[key])
+
+// }
+
+
+// const student = {
+//     name: "Egide",
+//     age:20,
+//     isGraduated: false,
+//     courses:["AMT","AGG","SOD"]
+// }
+
+// console.log(Object.keys(student).length)
+
+ const string = " hel  lo" 
+ const newStr = string.split("  ").join('')
+ console.log(newStr)
