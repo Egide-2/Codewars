528d795377d8cf197f9785ccf8fb4daeaea7dd40
Egide-2 <EgideBilly69@gmail.com>
2025-08-18 14:42:04 +0200

    how make a defalult parameter

--- index.js
+++ index.js
@@ -24,7 +24,62 @@
 // }
 
 // console.log(Object.keys(student).length)
+//  const arr = 'hello'
+//  const sum = arr.replace(arr.slice(3),"ggggg")
+//  console.log(sum)
 
- const string = " hel  lo" 
- const newStr = string.split("  ").join('')
- console.log(newStr)
+// const str = "0123456789"
+
+// for(let i = 0 ; i < str.length ;i++){
+// if (str[i]<  5 ) return "0"
+// else return "1"
+// console.log(str)
+// }
+
+// const person1={
+//     name:"egide billy",
+//     age:108,
+//     profession: "Deverloper",
+//     salary :"not have salary"
+// }
+
+// const person2 = {
+//     adress:"kigali"
+
+// }
+
+// const person3 = {...person1, ...person2 }
+
+// console.log(person3)
+
+
+// const jsonPerson = JSON.stringify(person3)
+
+// console.log(jsonPerson)
+
+// this is how can i get the index of the character in the code
+// const arr = "gyyyy"
+// console.log(arr.indexOf("g"))
+
+
+// const users = [
+//     {name:"billy",age:78,price:5678},
+//     {name:"martin",age:78,price:5678},
+//     {name:"emmy",age:78,price:5678}
+// ]
+
+// function sayHi(){
+//  if(users.map(users => users.name === "name"))
+
+
+// }
+
+// console.log(sayHi())
+
+
+class Ball {
+    getBall( ballType = "regular"){
+        this.ballType == ballType
+    }
+}
+
